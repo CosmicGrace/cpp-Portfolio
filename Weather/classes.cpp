05249d7e5cc625c03f90b8040085eff27f6dbf79
CosmicGrace <jason4618595@gmail.com>
2025-12-17 19:10:55 -0600

    Update classes.cpp

--- Weather/classes.cpp
+++ Weather/classes.cpp
@@ -1,4 +1,9 @@
 /* ****************************************
+⚠️ Copyright Notice
+This project is not open source. All rights reserved.
+No unauthorized use, modification, distribution, or commercial use permitted.
+Copyrighted to CosmicGrace (https://github.com/CosmicGrace)
+
 
 Filename: classes.cpp
 Author: CosmicGrace
@@ -107,3 +112,4 @@ void WeatherAnalyzer::displayHistory() const {
     TEXT
 
 ----------------------------- */
+
